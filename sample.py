62ff4dc1b3dfd775019285517d752ec88a7c9d9d
Philip Doldo <philipmdoldo@gmail.com>
2026-04-23 16:26:13 -0400

    added checkpoint arg

--- sample.py
+++ sample.py
@@ -10,20 +10,23 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, required=True)
+    parser.add_argument("--checkpoint", type=str, required=False)
     args = parser.parse_args()
 
     with open(args.config, "r") as f:
         config = yaml.safe_load(f)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
     tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-
     ctmc = UniformCTMC(config)
 
     model_config = DiTConfig.from_dict(config["model"])
     model = DiT(model_config) # this model outputs the log of the scores
     model.to(device)
+
+    if args.checkpoint:
+        checkpoint = torch.load(config["checkpoint_path"], map_location="cpu")
+        model.load_state_dict(checkpoint["model"])
 
     with torch.no_grad():
         tokens = ctmc.forward_euler_sample(log_score_model=model, batch_size=2, seq_len=1024, num_steps=1000, device=device)
